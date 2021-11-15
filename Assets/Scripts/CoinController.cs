f64fa6595d4e7d420e579b3fae204b2adc7e5736
MatroCholo <64357478+MatroCholo@users.noreply.github.com>
2021-11-15 17:22:49 +0300

    v0.1 INDEV

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//эта библиотека нужна для работы с интерфейсом
+using UnityEngine.UI;
+
+public class CoinController : MonoBehaviour {
+
+//ссылка на объект с текстом
+public GameObject TextObject;
+//ссылка на текстовый компонент
+Text textComponent;
+//звук поднятия монеты
+public AudioClip CoinSound;
+//переменная подсчета монет
+public int coin;
+
+//запустится один раз при запуске скрипта
+void Start () {
+//делаем линк на текстовый компонент, который находится на текстовом объекте
+textComponent = TextObject.GetComponent <Text> ();
+}
+
+//запустится если collider2D попал в триггер
+void OnTriggerEnter2D (Collider2D other) {
+//проверка, имеет-ли объект тэг Coin
+if (other.tag == "Coin") {
+//увеличить переменную подсчета монет
+coin = coin + 1;
+//записать в текст результат счета монет, преобразованный в текстовую переменную
+textComponent.text = coin.ToString();
+//проиграть звук поднятия монеты на позиции крысы
+AudioSource.PlayClipAtPoint (CoinSound, transform.position);
+//удалить монету из сцены
+Destroy (other.gameObject);
+}
+}
+}
